d509845b1a90f83f7c1434c5de0519976f005757
MeenugaRaghavendra1994 <meenuga.raghavendra@orchidsintl.edu.in>
2026-06-27 07:02:00 +0000

    Filename _ removed

--- backend/app/main.py
+++ backend/app/main.py
@@ -1563,9 +1563,10 @@ def download_challan_pdf(challan_id: str) -> Response:
     
     safe_customer_name = str(customer_name).replace("\xa0", " ").strip()
     safe_sender_name = str(sender_name).replace("\xa0", " ").strip()
+    
     file_name= f"{safe_sender_name}_{challan_number}_{safe_customer_name}.pdf"
     
-    headers = {"Content-Disposition": f'attachment;filename={file_name}"'}
+    headers = {"Content-Disposition": f'attachment; filename="{file_name}"'}
     return Response(content=pdf_bytes, media_type="application/pdf", headers=headers)
 
 def create_sspl_logo():
